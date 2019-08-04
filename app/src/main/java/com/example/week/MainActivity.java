039dd636a0d9aa9d157c621f034a884ee5c7360a
tejagalande <tejas5galande@gmail.com>
2019-08-04 16:09:04 +0530

    Creating layout using xml code

--- app/src/main/java/com/example/week/MainActivity.java
+++ app/src/main/java/com/example/week/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.week;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        MainActivity m = this;
+
+        LinearLayout.LayoutParams root = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(root);
+
+        LinearLayout.LayoutParams edt = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        EditText e = new EditText(m);
+        e.setLayoutParams(edt);
+        layout.addView(e);
+
+        LinearLayout.LayoutParams btn = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        Button b = new Button(this);
+        b.setLayoutParams(btn);
+        b.setText("Ok");
+        layout.addView(b);
+
+        setContentView(layout);
     }
 }
