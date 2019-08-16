30e8016365f14b9931fbefeea96f03d8240ec441
tejagalande <tejas5galande@gmail.com>
2019-08-16 09:50:32 +0530

    Event handling: Anonymous class defined

--- uixml/src/main/java/com/example/uixml/MainActivity.java
+++ uixml/src/main/java/com/example/uixml/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.uixml;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button btnok = (Button) findViewById(R.id.btnOK);
+        //btnok.setOnClickListener(new Click());
+        // anonymous name class
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText edtanyname = (EditText) findViewById(R.id.edtAnyName);
+                String data = edtanyname.getText().toString();
+                edtanyname.setText(data.toUpperCase());
+            }
+        });
     }
+        // name of class
+//    private class Click implements View.OnClickListener{
+//
+//        @Override
+//        public void onClick(View view) {
+//
+//        }
+//    }
 }
+
